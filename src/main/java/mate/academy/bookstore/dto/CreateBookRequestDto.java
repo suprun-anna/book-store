6ad1e1f9963db54c832d65da9c3bbaf3fba31204
Anna Suprun <evgenevnaanna989@gmail.com>
2024-02-11 20:51:08 +0200

    replaced dependency, replaced annotations

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -1,22 +1,22 @@
 package mate.academy.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
+    @NotBlank
     private String isbn;
     @NotNull
     @Min(0)
     private BigDecimal price;
-    @NotNull
     private String description;
-    @NotNull
     private String coverImage;
 }
